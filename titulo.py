9b22179c51da3e0d94d857bba32536ae5648d22a
Oscar-Gonz <65528098+Oscar-Gonz@users.noreply.github.com>
2020-05-18 14:17:51 -0500

    Revisión 2
    Actuailzación de proyecto.

--- titulo.py
+++ titulo.py
@@ -7,5 +7,37 @@ app = Flask(__name__)
 def url_principal():
 	return render_template("template.html" , nombre = "Oscar González")
 
+@app.route("/estudios")
+def url_estudios():
+ 	tipos = [
+ 		"Americano",
+ 		"Latte"
+ 	]
+ 	return render_template("menu.html", tipos=tipos, nombre="Oscar González")
+
+@app.route("/deporte")
+def url_deporte():
+ 	tipos = [
+ 		"Americano",
+ 		"Latte"
+ 	]
+ 	return render_template("deporte.html", tipos=tipos, nombre="Oscar González")
+
+@app.route("/ocio")
+def url_ocio():
+ 	tipos = [
+ 		"Americano",
+ 		"Latte"
+ 	]
+ 	return render_template("ocio.html", tipos=tipos, nombre="Oscar González")
+
+@app.route("/regresar")
+def url_regresar():
+ 	tipos = [
+ 		"Americano",
+ 		"Latte"
+ 	]
+ 	return render_template("template.html", tipos=tipos, nombre="Oscar González")
+
 if __name__ == "__main__":
 	app.run(debug = True)
